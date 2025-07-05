9f2abebf6abba50f3578deb377b33e475f49eaa6
Ysdfsd <sd>
2025-07-05 10:47:41 +0300

    Manifests are generated

--- cmd/main.go
+++ cmd/main.go
@@ -4,59 +4,89 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"strings"
 )
 
-func main() {
-	videoFileName := "input.mov"
-	outputDirectory := "segments"
+type Profile struct {
+	Resolution string
+	Bitrate    string
+}
 
+func transcodeInputVideo(videoFileName string, outputDirectory string, profile Profile) string {
 	if err := os.MkdirAll(outputDirectory, os.ModePerm); err != nil {
 		fmt.Println("Error creating output directory:", err)
+		return ""
 	}
 
-	profiles := []struct {
-		Resolution string
-		Bitrate    string
-	}{
+	outputPath := fmt.Sprintf("%s/output_%s_%s.mp4", outputDirectory, profile.Resolution, profile.Bitrate)
+
+	cmd := exec.Command(
+		"ffmpeg",
+		"-i", videoFileName,
+		"-vf", fmt.Sprintf("scale=%s", profile.Resolution),
+		"-b:v", profile.Bitrate,
+		"-c:a", "aac",
+		"-strict", "experimental",
+		outputPath,
+	)
+
+	if err := cmd.Run(); err != nil {
+		fmt.Println("Error while transcoding the video:", err)
+		return ""
+	}
+
+	fmt.Printf("Created %s\n", outputPath)
+	return outputPath
+}
+
+func generateManifestAndSegments(videoFilePath string, outputDirectory string, manifestFileName string) string {
+	if err := os.MkdirAll(outputDirectory, os.ModePerm); err != nil {
+		fmt.Println("Error creating output directory:", err)
+		return ""
+	}
+
+	cmd := exec.Command(
+		"ffmpeg",
+		"-i", videoFilePath,
+		"-c:a", "copy",
+		"-c:v", "copy",
+		"-map", "0",
+		"-f", "dash",
+		fmt.Sprintf("%s/%s", outputDirectory, manifestFileName),
+	)
+
+	if err := cmd.Run(); err != nil {
+		fmt.Println("Error while generating DASH manifest:", err)
+		return ""
+	}
+
+	fmt.Println("DASH manifest has been created successfully")
+	return fmt.Sprintf("%s/%s", outputDirectory, manifestFileName)
+}
+
+func main() {
+	videoFileName := "input.mp4"
+	outputDirectory := "segments"
+
+	profiles := []Profile{
 		{"1920x1080", "3000k"},
 		{"1280x720", "1500k"},
 		{"640x360", "500k"},
 	}
 
 	for _, profile := range profiles {
-		outputFileName := fmt.Sprintf("%s/output_%s_%s.mp4", outputDirectory, profile.Resolution, profile.Bitrate)
-		cmd := exec.Command(
-			"ffmpeg",
-			"-i", videoFileName,
-			"-vf", fmt.Sprintf("scale=%s", profile.Resolution),
-			"-b:v", profile.Bitrate,
-			"-c:a", "aac",
-			"-strict", "experimental",
-			outputFileName,
+		transcodedVideoPath := transcodeInputVideo(
+			videoFileName, 
+			outputDirectory,
+			profile,
 		)
 
-		if err := cmd.Run(); err != nil {
-			fmt.Println("Error while transcoding the video:", err)
+		if err := generateManifestAndSegments(
+			transcodedVideoPath,
+			strings.Replace(transcodedVideoPath, ".mp4", "", -1),
+			"manifest.mpd",
+		); err == "" {
 			return
 		}
-
-		fmt.Printf("Created %s\n", outputFileName)
 	}
-
-	cmd := exec.Command(
-		"ffmpeg",
-		"-f", "dash",
-		"-i", fmt.Sprintf("%s/output_%%v.mp4", outputDirectory),
-		"-map", "0",
-		"-f", "dash",
-		fmt.Sprintf("%s/manifest.mpd", outputDirectory),
-	)
-
-	if err := cmd.Run(); err != nil {
-		fmt.Println("Error while generating DASH manifest:", err)
-		return
-	}
-
-	fmt.Println("DASH manifest has been created successfully")
-
 }
